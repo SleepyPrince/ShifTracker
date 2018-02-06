16f4a81b61223e9d4ea323ac846f0c443ec3fd92
SleepyPrince <jackal.c@gmail.com>
2018-02-07 01:20:22 +0800

    Make dropbox token optional

--- dropbox.ps1
+++ dropbox.ps1
@@ -5,13 +5,17 @@ Function Dropbox {
 		[Parameter(Mandatory=$true)]
 		[string]$SourceFilePath,
 		[Parameter(Mandatory=$true)]
-		[string]$TargetFilePath
+		[string]$TargetFilePath,
+		[Parameter(Mandatory=$false)]
+		[string]$token
 	)
 
 	# Load Token
-	$token = Get-Content .\token -Raw
+	if(-not($PSBoundParameters.ContainsKey('token')) -and $token){
+		$token = Get-Content .\token -Raw
+	}
 	$authorization = "Bearer $($token)"
-
+	
 	$arg = '{ "path": "' + $TargetFilePath + '", "mode": {".tag" : "overwrite"} }'
 
 	# Dropbox Upload Headers
@@ -23,6 +27,15 @@ Function Dropbox {
 	# Get File Name
 	$name = Split-Path $SourceFilePath -leaf
 	
+	# Check if toLog exists, if not use Write-Host instead
+	if (Get-Command toLog -errorAction SilentlyContinue)
+	{
+		$alias = $false
+	}else{
+		Set-Alias toLog Write-Host
+		$alias = $true
+	}
+	
 	# Try to upload and write error if fail
 	try{
 		Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers > $null
@@ -33,5 +46,10 @@ Function Dropbox {
 		}else{
 			toLog "$(Get-Date)`t$($name) upload failed. (Reason unknown)"
 		}
+	}
+	
+	# remove alias if set
+	if($alias){
+		Remove-Item alias:\toLog
 	}
 }
