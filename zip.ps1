95b10a2740dc21d8e6ae6c0353f5a293f0269982
SleepyPrince <jackal.c@gmail.com>
2019-07-14 19:56:18 +0800

    Complete zip function (awaiting integration)

--- zip.ps1
+++ zip.ps1
@@ -3,9 +3,17 @@ $root = "D:\Users\cad\Documents\ShifTracker"
 get-item 'Z:\*Roster' | Foreach {if($_.LastWriteTime -gt $latest){$latest = $_.LastWriteTime}}
 $zip = "$root\$($latest.toString('yyyyMMddHHmm')).zip"
 
-# Zip file with supressed progress bar
-$global:ProgressPreference = 'SilentlyContinue'
-Compress-Archive -LiteralPath 'Z:\ATFSO_SATCO_ADCT Roster','Z:\ATCO & ADCO Roster' -DestinationPath $zip -CompressionLevel Optimal -Update | Out-Null
+# Check if file already exists
+If(-Not (Test-path $zip -PathType Leaf)){
+	# Zip file with supressed progress bar
+	$global:ProgressPreference = 'SilentlyContinue'
+	Compress-Archive -LiteralPath 'Z:\ATFSO_SATCO_ADCT Roster','Z:\ATCO & ADCO Roster' -DestinationPath $zip -CompressionLevel Optimal -Update | Out-Null
 
-# Print filename
-write-host $zip.split('\')[-1]
+	# Print filename
+	write-host $zip.split('\')[-1]
+
+	# Upload zip file
+	DropBox $zip "/$($zip.split('\')[-1])"
+}else{
+	write-host "File already exists ($($zip.split('\')[-1]))"
+}
